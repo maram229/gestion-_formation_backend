34936a0db530864b93863d31746154ea0ba312f9
unknown <maramsayhi31@gmail.com>
2026-01-13 11:47:58 +1100

    add gestion-_formation_bachend

---
+++ src/main/java/com/formations/demo/GestionFormationsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.formations.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormationsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFormationsbackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/formations/percistance/entities/AppUser.java
@@ -0,0 +1,63 @@
+package com.formations.percistance.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+
+public class AppUser {
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	private int idUser;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String poste;
+	private String telephone;
+	private String password;
+	
+	
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPoste() {
+		return poste;
+	}
+	public void setPoste(String poste) {
+		this.poste = poste;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
